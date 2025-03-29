e05d3ad38f601309868e38494e70485a19c39ce9
Steven Rotelli <italic_clatter.8d@icloud.com>
2025-03-28 22:21:44 -0500

    chore: css update

--- public/index.php
+++ public/index.php
@@ -12,10 +12,14 @@ require_once __DIR__ . '/../templates/header.php';
 
 <h1>Welcome, <?= htmlspecialchars($_SESSION['user']['name']?? 'Username not found') ?>!</h1>
 <p>You are logged in.</p>
-<a href="logout.php">Log out</a>
-<a href="changepw.php">Change Password</a>
-<?php if($_SESSION['user']['user_type'] === 'admin'):?>
-<a href="register.php">Register User</a>
-<?php endif; ?>
+<div class="screens">
+  <a href="logout.php">Log out</a>
+  <a href="changepw.php">Change Password</a>
+  <?php if($_SESSION['user']['user_type'] === 'admin'):?>
+    <a href="register.php">Register User</a>
+    <a href="delete.php">Delete User</a>
+    <a href="update.php">Update User</a>
+  <?php endif; ?>
+</div>
 
 <?php require_once __DIR__ . '/../templates/footer.php'; ?>
